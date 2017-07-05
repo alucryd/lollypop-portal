c0d9f6a320671b1ccf376a79fdcf9bd5fb00be1b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-07-05 15:42:13 +0200

    install dbus service on the correct path & make lollypop-portal +x

---
+++ meson_post_install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from compileall import compile_dir
+from os import environ, path
+from subprocess import call
+
+prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = path.join(prefix, 'share')
+destdir = environ.get('DESTDIR', '')
+
+if not destdir:
+    call(['chmod', '+x', path.join(prefix, 'libexec', 'lollypop-portal')])
